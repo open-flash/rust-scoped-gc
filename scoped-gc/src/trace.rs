72ead4235bd1525266fd91c92001dd3c2a3b28ae
Charles Samborski <demurgos@demurgos.net>
2018-03-10 17:54:06 +0100

    Add Trace implementations for more standard types

--- scoped-gc/src/trace.rs
+++ scoped-gc/src/trace.rs
@@ -37,84 +37,105 @@ pub unsafe trait Trace {
   unsafe fn unroot(&self);
 }
 
-unsafe impl Trace for u8 {
-  unsafe fn mark(&self) {}
-
-  unsafe fn root(&self) {}
-
-  unsafe fn unroot(&self) {}
+/// This macro rule implements `Trace` with empty functions.
+///
+/// Use this for types that can't contain other `Trace` types.
+#[macro_export]
+macro_rules! unsafe_empty_trace {
+  ($T: ty) => {
+    unsafe impl Trace for $T {
+      #[inline]
+      unsafe fn mark(&self) {}
+      #[inline]
+      unsafe fn root(&self) {}
+      #[inline]
+      unsafe fn unroot(&self) {}
+    }
+  }
 }
 
-unsafe impl Trace for String {
-  unsafe fn mark(&self) {}
+unsafe_empty_trace!(());
+unsafe_empty_trace!(u8);
+unsafe_empty_trace!(u16);
+unsafe_empty_trace!(u32);
+unsafe_empty_trace!(u64);
+unsafe_empty_trace!(usize);
+unsafe_empty_trace!(i8);
+unsafe_empty_trace!(i16);
+unsafe_empty_trace!(i32);
+unsafe_empty_trace!(i64);
+unsafe_empty_trace!(isize);
+unsafe_empty_trace!(f32);
+unsafe_empty_trace!(f64);
+unsafe_empty_trace!(char);
+unsafe_empty_trace!(String);
+unsafe_empty_trace!(::std::path::Path);
+unsafe_empty_trace!(::std::path::PathBuf);
+unsafe_empty_trace!(::std::sync::atomic::AtomicBool);
+unsafe_empty_trace!(::std::sync::atomic::AtomicIsize);
+unsafe_empty_trace!(::std::sync::atomic::AtomicUsize);
 
-  unsafe fn root(&self) {}
+#[macro_export]
+macro_rules! unsafe_custom_trace {
+  ($this:ident, $body:expr) => {
+    #[inline]
+    unsafe fn mark(&self) {
+      #[inline]
+      unsafe fn trace<T: $crate::Trace>(it: &T) { $crate::Trace::mark(it) }
+      let $this = self;
+      $body
+    }
+    #[inline]
+    unsafe fn root(&self) {
+      #[inline]
+      unsafe fn trace<T: $crate::Trace>(it: &T) { $crate::Trace::root(it) }
+      let $this = self;
+      $body
+    }
+    #[inline]
+    unsafe fn unroot(&self) {
+      #[inline]
+      unsafe fn trace<T: $crate::Trace>(it: &T) { $crate::Trace::unroot(it) }
+      let $this = self;
+      $body
+    }
+  }
+}
 
-  unsafe fn unroot(&self) {}
+unsafe impl<T: Trace> Trace for Box<T> {
+  unsafe_custom_trace!(this, {
+    trace(&**this)
+  });
 }
 
 unsafe impl<T: Trace> Trace for Option<T> {
-  unsafe fn mark(&self) {
-    match self {
-      &Some(ref x) => x.mark(),
-      &None => (),
-    }
-  }
+  unsafe_custom_trace!(this, {
+    if let Some(ref x) = *this { trace(x) }
+  });
+}
 
-  unsafe fn root(&self) {
-    match self {
-      &Some(ref x) => x.root(),
-      &None => (),
+unsafe impl<T: Trace, E: Trace> Trace for Result<T, E> {
+  unsafe_custom_trace!(this, {
+    match *this {
+      Ok(ref r) => trace(r),
+      Err(ref e) => trace(e),
     }
-  }
+  });
+}
 
-  unsafe fn unroot(&self) {
-    match self {
-      &Some(ref x) => x.unroot(),
-      &None => (),
+unsafe impl<T: Trace> Trace for Vec<T> {
+  unsafe_custom_trace!(this, {
+    for item in this {
+      trace(item)
     }
-  }
+  });
 }
 
 unsafe impl<K: Eq + ::std::hash::Hash + Trace, V: Trace> Trace for ::std::collections::HashMap<K, V> {
-  unsafe fn mark(&self) {
-    for (k, v) in self.iter() {
-      k.mark();
-      v.mark();
+  unsafe_custom_trace!(this, {
+    for (k, v) in this.iter() {
+      trace(k);
+      trace(v);
     }
-  }
-
-  unsafe fn root(&self) {
-    for (k, v) in self.iter() {
-      k.root();
-      v.root();
-    }
-  }
-
-  unsafe fn unroot(&self) {
-    for (k, v) in self.iter() {
-      k.unroot();
-      v.unroot();
-    }
-  }
-}
-
-unsafe impl<T: Trace> Trace for ::std::vec::Vec<T> {
-  unsafe fn mark(&self) {
-    for item in self.iter() {
-      item.mark();
-    }
-  }
-
-  unsafe fn root(&self) {
-    for item in self.iter() {
-      item.root();
-    }
-  }
-
-  unsafe fn unroot(&self) {
-    for item in self.iter() {
-      item.unroot();
-    }
-  }
+  });
 }
